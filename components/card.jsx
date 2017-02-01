b7e6994bf9a9a2ee8e50d77d2a7f23f972a9c2c2
Melissa Lau <mellauyellow@gmail.com>
2017-02-01 14:25:52 -0800

    add icons

--- components/card.jsx
+++ components/card.jsx
@@ -7,9 +7,21 @@ class Card extends React.Component {
 
   render() {
     const {card} = this.props;
+    let icon;
+    if (card.suit === 'hearts') {
+      icon = <i className={`flaticon-heart ${card.shown}`}></i>;
+    } else if (card.suit === 'spades') {
+      icon = <i className={`flaticon-spades ${card.shown}`}></i>;
+    } else if (card.suit === 'clubs') {
+      icon = <i className={`flaticon-clover ${card.shown}`}></i>;
+    } else {
+      icon = <i className={`flaticon-diamond ${card.shown}`}></i>;
+    }
+
     return (
       <div className={`card ${card.shown} ${card.matched}`} onClick={() => (this.props.updateGame(card))}>
         <h3 className={card.shown}>{card.value}</h3>
+        {icon}
       </div>
     );
   }
